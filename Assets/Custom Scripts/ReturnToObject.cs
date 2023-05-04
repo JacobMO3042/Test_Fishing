fecfb00b12497d15511866ebdc22655785ab8055
JacobMO3042 <115160743+JacobMO3042@users.noreply.github.com>
2023-05-04 14:44:13 +0100

    Reel in

--- Assets/Custom Scripts/ReturnToObject.cs
+++ Assets/Custom Scripts/ReturnToObject.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class ReturnToObject : MonoBehaviour
 {
     public Transform target;// object to return to
     public float speed; //movement per second
-    public bool Active = false; //toggle the return
+    public bool IsReelActive = false; //toggle the return
 
-    void ActiveReel(bool Active){
-        Active = true;
+    public void Activate()
+    {
+        IsReelActive = true;
     }
-    void DeactiveReel(bool Active){
-        Active = false;
+    public void Deactive()
+    {
+        IsReelActive = false;
     }
 
-
+    void ReelIn()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -26,14 +32,13 @@ public class ReturnToObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(Active = true)
-        // {
-        //     void ReelIn(){
-        //         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        //     } 
-        // }
+    if (IsReelActive)
+        {
+            ReelIn();
+            Debug.Log("Swag");
+        }
 
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
 
 }
 
